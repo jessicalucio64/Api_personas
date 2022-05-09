3c7c1a6c3789dadb90600e5c6c1881f4f3be30f7
TeresitaReyna <teresitareyna.epsanol@hotmail.com>
2022-05-09 11:24:50 -0500

    Carpeta del ejercico by Teresita xd

---
+++ teresita/app.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('#searchInput');
+const userList = document.querySelector('#users');
+let users = [];
+
+let url = "https://fakerapi.it/api/v1/users?_quantity=1000";
+
+document.addEventListener('DOMContentLoaded', async () => {
+    userList.innerHTML = `<h1>Cargando...</h1>`;
+    const data = await loadUsers();
+    users = data.data;
+    // console.log(users);
+    renderUsers(users);
+});
+
+input.addEventListener('keyup', e => {
+    const newUsers = users.filter(user => `${user.firstname.toLowerCase()} ${user.lastname.toLowerCase()}`.includes(input.value.toLowerCase()));
+    renderUsers(newUsers);
+});
+
+async function loadUsers(){
+    let respuesta = await fetch(url);
+    return res = respuesta.json();
+    //console.log(res);
+}
+
+const createUserItems = users => users.map(user => `<li class="bg-zinc-800 hover:bg-zinc-700 hover:cursor-pointer">${user.firstname} ${user.lastname}</li>`).join(' ');
+
+function renderUsers(users) {
+    const itemsString = createUserItems(users);
+    userList.innerHTML = itemsString;
+}
